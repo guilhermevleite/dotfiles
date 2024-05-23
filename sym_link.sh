7b89e3b286b38e3f8a3948321062cc61e193e2fa
Guilherme Leite <guilherme.vieira.leite@gmail.com>
2024-05-23 14:25:37 -0300

    symlink paths

---
+++ sym_link.sh
@@ -0,0 +1,4 @@
+ln -s ~/.dotfiles/kitty/kitty.config ~/.config/kitty/kitty.config
+ln -s ~/.dotfiles/fish/config.fish ~/.config/fish/config.fish
+ln -s ~/.dotfiles/neovim/lazyvim/init.lua ~/.config/nvim/init.lua
+ln -s ~/.dotfiles/neovim/lazyvim/plugins/neorg.lua ~/.config/nvim/lua/plugins/neorg.lua
