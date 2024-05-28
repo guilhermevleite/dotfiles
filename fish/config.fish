84d4190b54c4ae040cc8b6bc9572f3f241c37023
gui <guilherme.vieira.leite@gmail.com>
2024-05-28 09:51:13 -0300

    NEW add starship to fish shell
    Signed-off-by: gui <guilherme.vieira.leite@gmail.com>

--- fish/config.fish
+++ fish/config.fish
@@ -6,6 +6,7 @@ export PATH="$PATH:/opt/nvim/"
 
 # >>> conda initialize >>>
 # !! Contents within this block are managed by 'conda init' !!
-eval /home/leite/miniconda3/bin/conda "shell.fish" "hook" $argv | source
+eval /home/leite/miniconda3/bin/conda "shell.fish" hook $argv | source
 # <<< conda initialize <<<
 
+starship init fish | source
