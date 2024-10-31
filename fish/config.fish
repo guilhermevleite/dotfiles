c2ac71559c45040e2cd7e87da00b72d086522b70
Guilherme Leite <guilherme.vieira.leite@gmail.com>
2024-10-31 13:39:39 -0300

    fish and kitty configs

--- fish/config.fish
+++ fish/config.fish
@@ -6,9 +6,4 @@ alias latex="latexmk -pvc -f -pdf"
 
 export PATH="$PATH:/opt/nvim/"
 
-# >>> conda initialize >>>
-# !! Contents within this block are managed by 'conda init' !!
-eval /home/leite/miniconda3/bin/conda "shell.fish" hook $argv | source
-# <<< conda initialize <<<
-
 starship init fish | source
--- sym_link.sh
+++ sym_link.sh
@@ -1,4 +1,4 @@
 ln -sfn ~/.dotfiles/kitty/kitty.conf ~/.config/kitty/kitty.conf
 ln -sfn ~/.dotfiles/fish/config.fish ~/.config/fish/config.fish
 ln -sfn ~/.dotfiles/neovim/lazyvim/init.lua ~/.config/nvim/init.lua
-ln -sfn ~/.dotfiles/neovim/lazyvim/plugins/neorg.lua ~/.config/nvim/lua/plugins/neorg.lua
+ln -sfn ~/.dotfiles/starship/starship.toml ~/.config/startship.toml
