3c39b239e5ed9ebc5fc4188f45c7ec8fd1c3b237
Guilherme Leite <guilherme.vieira.leite@gmail.com>
2025-06-13 12:47:27 -0300

    pokemon added to fish

--- fish/config.fish
+++ fish/config.fish
@@ -7,3 +7,18 @@ alias latex="latexmk -pvc -f -pdf"
 export PATH="$PATH:/opt/nvim/"
 
 starship init fish | source
+
+pokemon -r {kanto,johto,hoenn} -l 0.8
+
+# >>> conda initialize >>>
+# !! Contents within this block are managed by 'conda init' !!
+if test -f /home/leite/miniconda3/bin/conda
+    eval /home/leite/miniconda3/bin/conda "shell.fish" hook $argv | source
+else
+    if test -f "/home/leite/miniconda3/etc/fish/conf.d/conda.fish"
+        . "/home/leite/miniconda3/etc/fish/conf.d/conda.fish"
+    else
+        set -x PATH /home/leite/miniconda3/bin $PATH
+    end
+end
+# <<< conda initialize <<<
