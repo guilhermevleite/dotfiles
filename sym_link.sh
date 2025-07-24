85e7ea39217ba11e620bd8108ca7e0564753e612
Guilherme Leite <guilherme.vieira.leite@gmail.com>
2025-07-24 14:39:55 -0300

    setup scripts

--- sym_link.sh
+++ sym_link.sh
@@ -2,3 +2,7 @@ ln -sfn ~/.dotfiles/kitty/kitty.conf ~/.config/kitty/kitty.conf
 ln -sfn ~/.dotfiles/fish/config.fish ~/.config/fish/config.fish
 ln -sfn ~/.dotfiles/neovim/lazyvim/init.lua ~/.config/nvim/init.lua
 ln -sfn ~/.dotfiles/starship/starship.toml ~/.config/startship.toml
+ln -sfn ~/.dotfiles/inputrc ~/.inputrc
+ln -sfn ~/.dotfiles/gitconfig ~/.gitconfig
+
+chsh -s /usr/bin/fish
