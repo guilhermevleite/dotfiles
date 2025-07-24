ccb056e17ea4d86a371a2ab31ee26dbd5bcfe16a
Guilherme Leite <guilherme.vieira.leite@gmail.com>
2025-07-24 15:18:43 -0300

    moved command from linux.sh into quick_set_linux, deleted unsed files

--- quick_set_linux.sh
+++ quick_set_linux.sh
@@ -7,3 +7,6 @@ ln -sfn ~/.dotfiles/ssh_config ~/.ssh/config
 # VIM
 ln -sfn ~/.dotfiles/neovim/lazyvim/init.lua ~/.config/nvim/init.lua
 ln -sfn ~/.dotfiles/neovim/lazyvim/keymaps.lua ~/.config/nvim/lua/config/keymaps.lua
+
+sudo apt-get update -y
+sudo apt-get install git curl htop psensor -y
