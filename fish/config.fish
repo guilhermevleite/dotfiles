93f442a94999999110adb63942be714a0291457f
Guilherme Leite <guilherme.vieira.leite@gmail.com>
2025-08-25 04:55:19 -0300

    updating latexmk to separate aux files from the main project
    Signed-off-by: Guilherme Leite <guilherme.vieira.leite@gmail.com>

--- fish/config.fish
+++ fish/config.fish
@@ -2,7 +2,9 @@ if status is-interactive
     # Commands to run in interactive sessions can go here
 end
 
-alias latex="latexmk -pvc -f -pdf"
+set -g fish_greeting
+
+alias latex="latexmk -auxdir=./.tmp -pvc -f -pdf"
 alias lt="du -sh * | sort -h"
 
 export PATH="$PATH:/opt/nvim/"
